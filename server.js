a257cc49e5f14053501d96bd9eaa90221157346e
Brian Olson <bwolson31@gmail.com>
2024-04-04 19:54:22 -0500

    adjusted logic for signup/login, as we're going to have two pages for that, added logic to authController.js, and filled out server.js with content related to routes.

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const app = express();
-const PORT = 3000; // You can choose any port number
+const session = require('express-session');
+const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = requre('connect-session-sequelize')
+(session.Store);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  // TODO: logic for sess
+}
+
+// TODO: logic for handlebars
+
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync Sequelize models with the database
+sequelize.sync({ force: false }).then(() => {
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
