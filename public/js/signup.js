f86c9c331ba6efc3d170f9c670e2b6098f160f9c
fruitxxsnackz <153016953+fruitxxsnackz@users.noreply.github.com>
2024-04-10 23:28:28 +0000

    New Official Branch

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,41 +1,11 @@
-// const signupBtn = document.getElementById('submit-signup');
+const signupBtn = document.getElementById('submit-signup');
 
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
+const signupForm = async (event) => {
+    event.preventDefault();
 
-//     const username = document.getElementById('name-signup').value.trim();
-//     const password = document.getElementById('password-signup').value.trim();
-//     const email = document.getElementById('email-signup').value.trim();
-
-//     if (username && password && email) {
-//         const response = await fetch('/api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ username, password, email }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-
-//         if (response.ok) {
-//             document.location.replace('/homepage');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// };
-
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const signupFormHandler = async (event) => {
-        event.preventDefault();
-
-        const username = document.getElementById('username-signup').value.trim();
-        const password = document.getElementById('password-signup').value.trim();
-        const email = document.getElementById('email-signup').value.trim();
+    const username = document.getElementById('username-signup').value.trim();
+    const password = document.getElementById('password-signup').value.trim();
+    const email = document.getElementById('email-signup').value.trim();
 
     if (username && password && email) {
         const response = await fetch('/api/users', {
@@ -53,7 +23,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 };
 
-    document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-});
 
 signupBtn.addEventListener('submit', signupForm);
